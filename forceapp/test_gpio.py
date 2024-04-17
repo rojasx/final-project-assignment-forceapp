69777915888bf0265486b199024838692828e0f7
rojasx <rojasx@colorado.edu>
2024-04-17 11:45:10 -0600

    added test_gpio.py

---
+++ forceapp/test_gpio.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as gpio
+import time
+
+# Toggles our state
+def toggle_out(state):
+    new_state = 0
+    if state == gpio.LOW:
+        new_state == gpio.HIGH
+    else:
+        new_state == gpio.LOW
+    return new_state
+
+# Vars
+num_tests = 10
+t_dwell = 0.001
+pin_out = 37
+pin_in = 38
+
+# Setup
+gpio.setmode(gpio.BOARD)
+gpio.setup(pin_out, gpio.OUT, initial=gpio.LOW)
+gpio.setup(pin_in, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+
+# Toggle output and read, assert the output is what we expect
+# NEEDS WIRE CONNECTING PINS
+state = gpio.LOW
+for i in range(num_tests):
+    gpio.output(pin_out, state)
+    time.sleep(t_dwell)
+    assert gpio.input(pin_in) == state, "GPIO input did not match expected state."
+    time.sleep(t_dwell)
+    state = toggle_out(state)
+
+gpio.cleanup()
+print("GPIO Test Success.")
