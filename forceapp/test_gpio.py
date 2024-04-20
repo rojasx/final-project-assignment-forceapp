fef6dcc02fca621fe8af0936cc4d5a1798f51e60
rojasx <rojasx@colorado.edu>
2024-04-20 13:49:27 -0600

    fixed test_gpio.py

--- forceapp/test_gpio.py
+++ forceapp/test_gpio.py
@@ -5,9 +5,9 @@ import time
 def toggle_out(state):
     new_state = 0
     if state == gpio.LOW:
-        new_state == gpio.HIGH
+        new_state = gpio.HIGH
     else:
-        new_state == gpio.LOW
+        new_state = gpio.LOW
     return new_state
 
 # Vars
@@ -18,18 +18,23 @@ pin_in = 38
 
 # Setup
 gpio.setmode(gpio.BOARD)
-gpio.setup(pin_out, gpio.OUT, initial=gpio.LOW)
+gpio.setup(pin_out, gpio.OUT)
 gpio.setup(pin_in, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
 # Toggle output and read, assert the output is what we expect
 # NEEDS WIRE CONNECTING PINS
 state = gpio.LOW
-for i in range(num_tests):
-    gpio.output(pin_out, state)
-    time.sleep(t_dwell)
-    assert gpio.input(pin_in) == state, "GPIO input did not match expected state."
-    time.sleep(t_dwell)
-    state = toggle_out(state)
+try:
+    for i in range(num_tests):
+        gpio.output(pin_out, state)
+        time.sleep(t_dwell)
+        assert gpio.input(pin_in) == state, "GPIO input did not match expected state."
+        time.sleep(t_dwell)
+        state = toggle_out(state)
+except AssertionError:
+    print("GPIO Test Failed.")
+    gpio.cleanup()
+    exit(1)
 
 gpio.cleanup()
 print("GPIO Test Success.")
