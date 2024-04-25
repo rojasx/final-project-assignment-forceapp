fdba673e91e65e6841391bd1d940cde0ef58d301
rojasx <rojasx@colorado.edu>
2024-04-24 21:04:24 -0600

    rpi_server loops forever, even when cancelled.

--- forceapp/rpi_server.py
+++ forceapp/rpi_server.py
@@ -42,48 +42,53 @@ t_dwell = 0.1
 pin_clk = 37
 pin_data = 38
 
-# GPIO setup
-gpio.setmode(gpio.BOARD)
-gpio.setup(pin_clk, gpio.OUT, initial=gpio.LOW)
-gpio.setup(pin_data, gpio.IN)
-time.sleep(0.25)
+while (True):
+    # GPIO setup
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(pin_clk, gpio.OUT, initial=gpio.LOW)
+    gpio.setup(pin_data, gpio.IN)
+    time.sleep(0.25)
 
-# Socket setup
-server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((my_address, rfcomm_channel))
-server.listen(1)
-client, addr = server.accept()
-print("Accepted.")
+    # Socket setup
+    server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((my_address, rfcomm_channel))
+    server.listen(1)
+    client, addr = server.accept()
+    print("Accepted.")
 
-try:
-    handshake = client.recv(1024).decode('utf-8')
-    if handshake != "start":
-        print("Failed handshake.")
-        raise ValueError
-    print("About to stream data.")
-    while True:
-        # Stream data
-        tc_value = 0
-        tmp = 0
-        for bit in range(size_word):
-            clk_pulse(pin_clk)
-            tmp = gpio.input(pin_data)
+    try:
+        handshake = client.recv(1024).decode('utf-8')
+        if handshake != "start":
+            print("Failed handshake.")
+            raise ValueError
+        print("About to stream data.")
+        while True:
+            # Stream data
+            tc_value = 0
+            tmp = 0
+            for bit in range(size_word):
+                clk_pulse(pin_clk)
+                tmp = gpio.input(pin_data)
 
-            # Stuff the word
-            tc_value |= tmp << (size_word - bit -1)
+                # Stuff the word
+                tc_value |= tmp << (size_word - bit -1)
 
-        # Gain pulses
-        for pulse in range(gain):
-            clk_pulse(pin_clk)
+            # Gain pulses
+            for pulse in range(gain):
+                clk_pulse(pin_clk)
 
-        # print(int_to_pounds(unsigned_to_signed24(tc_value)))
-        client.send(str(int_to_pounds(unsigned_to_signed24(tc_value))).encode('utf-8'))
-        time.sleep(t_dwell)
-        
-except Exception as e:
-    print(f"Caught Exception: {e}.")
-    print("Closing socket.")
-    gpio.cleanup()
-    client.close()
-    server.close()
+            # print(int_to_pounds(unsigned_to_signed24(tc_value)))
+            client.send(str(int_to_pounds(unsigned_to_signed24(tc_value))).encode('utf-8'))
+            time.sleep(t_dwell)
+    except KeyboardInterrupt:
+        print("\nProgram exited by user.")
+        pass
+    except Exception as e:
+        print(f"\nCaught Exception: {e}.")
+        pass
+    finally:
+        print("Closing socket.")
+        gpio.cleanup()
+        client.close()
+        server.close()
