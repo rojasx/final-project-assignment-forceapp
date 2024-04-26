37c7d4394fd19ecedca453deed81b0d7fe20ab74
rojasx <rojasx@colorado.edu>
2024-04-25 21:46:24 -0600

    decided on best sequence for bluetooth startup

--- forceapp/forceapp_startup.sh
+++ forceapp/forceapp_startup.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-sleep 2
-bluetoothctl --agent NoInputNoOutput
-sleep 2
+# Configure bluetooth settings
+bluetoothctl agent off
+sleep 1
+bluetoothctl agent NoInputNoOutput
+sleep 1
+bluetoothctl pairable on
+sleep 1
 bluetoothctl discoverable on
-sleep 2
+sleep 1
+
+# Run the app
 python3 /usr/share/misc/rpi_server.py
